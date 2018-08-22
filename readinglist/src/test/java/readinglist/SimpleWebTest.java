c6dd54590caaf9c55a6f60ba414634e401a021a1
akartkam <akartkam@gmail.com>
2018-08-22 22:34:52 +0300

    Signed-off-by: akartkam <akartkam@gmail.com>

--- readinglist/src/test/java/readinglist/SimpleWebTest.java
+++ readinglist/src/test/java/readinglist/SimpleWebTest.java
@@ -2,6 +2,10 @@ package readinglist;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,22 +15,57 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class SimpleWebTest {
+
 	@Value("${local.server.port}")
 	private int port;
-	
+
+	private static FirefoxDriver browser;
+
 	@Test(expected = HttpClientErrorException.class)
 	public void pageNotFound() {
 		try {
 			RestTemplate rest = new RestTemplate();
-			rest.getForObject("http://localhost:{port}/bogusPage/www", String.class, port);
+			rest.getForObject("http://localhost:{port}/bogusPage/www",
+					String.class, port);
 			fail("Should result in HTTP 404");
 		} catch (HttpClientErrorException e) {
 			assertEquals(HttpStatus.NOT_FOUND, e.getStatusCode());
 			throw e;
 		}
 	}
+
+	@BeforeClass
+	public static void openBrowser() {
+		browser = new FirefoxDriver();
+		browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	@AfterClass
+	public static void closeBrowser() {
+		browser.quit();
+	}
+
+	@Test
+	public void addBookToEmptyList() {
+		String baseUrl = "http://localhost:" + port+"/readinkList";
+		browser.get(baseUrl);
+		assertEquals("You have no books in your book list", browser
+				.findElementByTagName("div").getText());
+		browser.findElementByName("title").sendKeys("BOOK TITLE");
+		browser.findElementByName("author").sendKeys("BOOK AUTHOR");
+		browser.findElementByName("isbn").sendKeys("1234567890");
+		browser.findElementByName("description").sendKeys("DESCRIPTION");
+		browser.findElementByTagName("form").submit();
+		WebElement dl = browser.findElementByCssSelector("dt.bookHeadline");
+		assertEquals("BOOK TITLE by BOOK AUTHOR (ISBN: 1234567890)", dl.getText());
+		WebElement dt = browser.findElementByCssSelector("dd.bookDescription");
+		assertEquals("DESCRIPTION", dt.getText());
+	}
+
 }
