b7834ab068b97f17268931ef6837047eda586648
akartkam <akartkam@gmail.com>
2018-08-24 17:21:33 +0300

    Signed-off-by: akartkam <akartkam@gmail.com>

--- readinglist/src/main/java/readinglist/ReadinglistApplication.java
+++ readinglist/src/main/java/readinglist/ReadinglistApplication.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.Ordered;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,23 +17,26 @@ import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class ReadinglistApplication implements WebMvcConfigurer {
+public class ReadinglistApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReadinglistApplication.class, args);
 	}
 	
-    @Override
+	@Override
     public void addViewControllers(ViewControllerRegistry registry) {
       registry.addViewController("/login").setViewName("login");
+      registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
     
     @Override
@@ -40,107 +44,5 @@ public class ReadinglistApplication implements WebMvcConfigurer {
         List<HandlerMethodArgumentResolver> argumentResolvers) {
       argumentResolvers.add(new ReaderHandlerMethodArgumentResolver());
     }
-
-	@Override
-	public void configurePathMatch(PathMatchConfigurer configurer) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void configureContentNegotiation(
-			ContentNegotiationConfigurer configurer) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void configureDefaultServletHandling(
-			DefaultServletHandlerConfigurer configurer) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addFormatters(FormatterRegistry registry) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void configureViewResolvers(ViewResolverRegistry registry) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addReturnValueHandlers(
-			List<HandlerMethodReturnValueHandler> returnValueHandlers) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void configureMessageConverters(
-			List<HttpMessageConverter<?>> converters) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void configureHandlerExceptionResolvers(
-			List<HandlerExceptionResolver> exceptionResolvers) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void extendHandlerExceptionResolvers(
-			List<HandlerExceptionResolver> exceptionResolvers) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Validator getValidator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public MessageCodesResolver getMessageCodesResolver() {
-		// TODO Auto-generated method stub
-		return null;
-	}
     
 }
--- readinglist/src/main/java/readinglist/SecurityConfig.java
+++ readinglist/src/main/java/readinglist/SecurityConfig.java
@@ -19,46 +19,36 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	  @Autowired
-	  private ReaderRepository readerRepository;
-	  
-	  @Override
-	  protected void configure(HttpSecurity http) throws Exception {
-	    http
-	      .authorizeRequests()
-	        .antMatchers("/").access("hasRole('READER')")
-	        //.antMatchers("/").permitAll()
-	        .antMatchers("/**").permitAll()
-	      .and()
-	      .csrf().disable()
-	      .formLogin()
-	        .loginPage("/login").permitAll()
-	        .failureUrl("/login?error=true");
-	  }
-	  
-	  @Override
-	  protected void configure(
-	              AuthenticationManagerBuilder auth) throws Exception {
-	    auth
-	      .userDetailsService(userDetailsService());
-	  }
-	  
-	  @Bean
-	  public UserDetailsService userDetailsService() {
-	    return new UserDetailsService() {
-	      @Override
-	      public UserDetails loadUserByUsername(String username)
-	          throws UsernameNotFoundException {
-	      	UserDetails userDetails = null;
-	    	Optional<Reader> r= Optional.of(readerRepository.findOne(username));
-	        if (r.isPresent()) userDetails = r.get(); 
-	        if (userDetails != null) {
-	          return userDetails;
-	        }
-	        throw new UsernameNotFoundException("User '" + username + "' not found.");
-	      }
-	    };
-	  }
+  @Autowired
+  private ReaderRepository readerRepository;
   
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .authorizeRequests()
+        .antMatchers("/").access("hasRole('READER')")
+        .antMatchers("/**").permitAll()
+      .and()
+      .formLogin()
+        .loginPage("/login")
+        .failureUrl("/login?error=true");
+  }
   
+  @Override
+  protected void configure(
+              AuthenticationManagerBuilder auth) throws Exception {
+    auth
+      .userDetailsService(new UserDetailsService() {
+        @Override
+        public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+          UserDetails userDetails = readerRepository.findOne(username);
+          if (userDetails != null) {
+            return userDetails;
+          }
+          throw new UsernameNotFoundException("User '" + username + "' not found.");
+        }
+      });
+  }
+
 }
